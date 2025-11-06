cca83091aecafcd4c1bc479fde347ddca63a69d7
GitHub Action <action@github.com>
2025-11-06 20:54:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/krowikbobton/searchstringoccurrencesintellijplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/krowikbobton/searchstringoccurrencesintellijplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.krowikbobton.searchstringoccurrencesintellijplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
